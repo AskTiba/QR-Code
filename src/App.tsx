cd9a2138540be987708b6b382a3aa1f3379b05ce
AskTiba <132336450+AskTiba@users.noreply.github.com>
2023-11-03 02:03:51 +0300

    Added QR-Code image

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import code from './assets/image-qr-code.png'
 function App() {
   return (
     <>
-      <div className="h-screen w-screen bg-Grayish-blue flex items-center justify-center">Shit is about to go down</div>
+      <div className="h-screen w-screen bg-Grayish-blue flex items-center justify-center">
+        <div className="">
+          <img src={code} alt="" />
+        </div>
+      </div>
     </>
   );
 }
