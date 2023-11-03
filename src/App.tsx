eb815d2856e33500de40785a9f421e7d8e3a1556
AskTiba <132336450+AskTiba@users.noreply.github.com>
2023-11-03 18:09:43 +0300

    Font-family and more styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,19 @@ function App() {
   return (
     <>
       <div className="h-screen w-screen bg-Light-gray flex items-center justify-center">
-        <div className="bg-White rounded-[15px] w-[300px]">
+        <div className="bg-White rounded-[25px] w-[300px]">
           <img className="p-[12px] rounded-[25px]" src={code} alt="" />
           <div className="flex flex-col justify-center items-center">
             <p
-              className="flex flex-col text-[15px] text-black font-bold
+              className="flex flex-col text-[20px] text-Dark-blue font-bold
             justify-center items-center my-[10px] "
             >
               Improve your front-end{" "}
               <span className="">skills by building projects</span>
             </p>
             <p
-              className="flex flex-col text-[15px] text-black font-normal 
-            justify-center items-center mb-[10px]"
+              className="flex flex-col text-[15px] text-Grayish-blue font-normal 
+            justify-center items-center mb-[30px]"
             >
               Scan the QR code to visit Frontend{" "}
               <span className="">Mentor and take your coding skills to</span>{" "}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundColor: {
-        "Grayish-blue": "hsl(220, 15%, 55%)",
-        "Light-gray": "hsl(212, 45%, 89%)",
+      colors: {
         "Dark-blue": "hsl(218, 44%, 22%)",
-        White: "hsl(0, 0%, 100%)",
+        "Grayish-blue": "hsl(220, 15%, 55%)",
+      },
+      backgroundColor: {
+        "Light-gray": "hsl(212, 45%, 89%)",
+        "White": "hsl(0, 0%, 100%)",
       },
     },
   },
