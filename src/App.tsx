ea1999747aac59884d09a67cb38f41ddd1a16447
AskTiba <132336450+AskTiba@users.noreply.github.com>
2023-11-03 14:41:05 +0300

    Added text paragraphs and styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import code from './assets/image-qr-code.png'
+import code from "./assets/image-qr-code.png";
 function App() {
   return (
     <>
-      <div className="h-screen w-screen bg-Grayish-blue flex items-center justify-center">
-        <div className="">
-          <img src={code} alt="" />
+      <div className="h-screen w-screen bg-Light-gray flex items-center justify-center">
+        <div className="bg-White rounded-[15px] w-[300px]">
+          <img className="p-[10px] rounded-[15px]" src={code} alt="" />
+          <div className="flex flex-col justify-center items-center">
+            <p
+              className="flex flex-col text-[15px] text-black font-bold
+            justify-center items-center my-[10px] "
+            >
+              Improve your front-end{" "}
+              <span className="">skills by building projects</span>
+            </p>
+            <p
+              className="flex flex-col text-[15px] text-black font-normal 
+            justify-center items-center mb-[10px]"
+            >
+              Scan the QR code to visit Frontend{" "}
+              <span className="">Mentor and take your coding skills to</span>{" "}
+              the next level
+            </p>
+          </div>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundColor: {
-        'Grayish-blue': 'hsl(220, 15%, 55%)',
+        "Grayish-blue": "hsl(220, 15%, 55%)",
+        "Light-gray": "hsl(212, 45%, 89%)",
+        "Dark-blue": "hsl(218, 44%, 22%)",
+        White: "hsl(0, 0%, 100%)",
       },
     },
   },
   plugins: [],
-}
+};
